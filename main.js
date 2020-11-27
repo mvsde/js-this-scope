a3f19efa0c498247866be8af1b536f60006d88a8
Fynn Becker <fynn@hey.com>
2020-11-27 09:33:07 +0100

    feat: Add class scope

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 
 
 console.log('Global scope:')
-console.log(this)
+console.log('  ', this)
 
 
 console.log('\nFunction scope:')
 
 function doTheThing () {
-  console.log(this)
+  console.log('  ', this)
 }
 
 doTheThing()
@@ -21,13 +21,32 @@ const stuff = {
   answer: 42,
 
   calculateQuestion () {
-    console.log('Normal:', this)
+    console.log('  Normal:', this)
   },
 
   getUniverse: () => {
-    console.log('Arrow:', this)
+    console.log('  Arrow:', this)
   }
 }
 
 stuff.calculateQuestion()
 stuff.getUniverse()
+
+
+console.log('\nClass scope:')
+
+class ClickyThing {
+  constructor (button, secret) {
+    this.secret = secret
+
+    button.addEventListener('click', function () {
+      console.log('  Normal:', this)
+    })
+
+    button.addEventListener('click', () => {
+      console.log('  Arrow:', this)
+    })
+  }
+}
+
+new ClickyThing(document.querySelector('button'), 42)
